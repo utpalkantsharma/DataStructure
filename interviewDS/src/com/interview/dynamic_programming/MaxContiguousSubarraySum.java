a667f371f7da728c8349e32269eba5824b3eb81c
Utpal.Kant <Utpal.Kant@L-ILB-UTPALK.247-inc.com>
2020-02-28 16:51:07 +0530

    Added linear approach method.

--- interviewDS/src/com/interview/dynamic_programming/MaxContiguousSubarraySum.java
+++ interviewDS/src/com/interview/dynamic_programming/MaxContiguousSubarraySum.java
@@ -1,15 +1,21 @@
 package com.interview.dynamic_programming;
 
+import java.util.Arrays;
+
 public class MaxContiguousSubarraySum {
 
 	public static void main(String[] args) {
 		int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 		System.out.println(cubicTimeApproach(arr));
+		System.out.println(linearApproach(arr));
 	}
 
 	// Brute force approach
+	// n(n+1)/2 = 9(10)/2 = 45 combinations
 	public static int cubicTimeApproach(int arr[]) {
 		int maxSum = Integer.MIN_VALUE;
+		int temp[] = new int[(arr.length * (arr.length + 1)) / 2];
+		int counter = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i; j < arr.length; j++) {
@@ -17,16 +23,29 @@ public class MaxContiguousSubarraySum {
 				for (int k = i; k <= j; k++) {
 					tempSum = tempSum + arr[k];
 				}
-				if (tempSum > maxSum)
-					maxSum = tempSum;
-
+				temp[counter++] = tempSum;
+				maxSum = Math.max(tempSum, maxSum);
 			}
 		}
+
+		System.out.println("Total Combination's = " + temp.length);
+		System.out.println(Arrays.toString(temp));
 		return maxSum;
 	}
 
 	// Dynamic programming
-	public static void linearApproach(int arr[]) {
+	public static int linearApproach(int arr[]) {
+		int sumArr[] = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i == 0) {
+				sumArr[i] = arr[i];
+			} else {
+				sumArr[i] = Math.max(arr[i], sumArr[i - 1] + arr[i]);
+			}
+		}
+		
+		return Arrays.stream(sumArr).max().getAsInt();
 
 	}
 
